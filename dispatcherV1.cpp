20a6bf33d5a1ced72dd2b3f6d4b8ab08b5411d24
User Aleksandr <pereverzevx@yandex.ru>
2026-07-07 23:31:03 +0300

    Добавьте файлы проекта.

---
+++ dispatcherV1.cpp
@@ -0,0 +1,12 @@
+﻿// dispatcherV1.cpp: определяет точку входа для приложения.
+//
+
+#include "dispatcherV1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
